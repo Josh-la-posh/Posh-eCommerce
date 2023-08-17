712e5c77e3952a8cfc04657f8a93a682f9816bcf
Joshua <joshuamayowa23@yahoo.com>
2023-08-17 12:46:25 +0100

    account popup updated

--- src/compoent/HeaderComponents/NavRight/Account.jsx
+++ src/compoent/HeaderComponents/NavRight/Account.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const PopUup = () => {
+  return (
+    <div className="popUp">
+      <div className="popUpLink">Login</div>
+      <div className="popUpLink">Sign Up</div>
+    </div>
+  );
+};
+
 export const Account = () => {
-    return (
-        <a href='/home' className="menu">
-            <span className='icon icon-left'><FontAwesomeIcon icon='user' /></span>
-                Account
-            <span className='icon'><FontAwesomeIcon icon='chevron-down' /></span>
-        </a>
-    )
-}
+  const [show, setShow] = useState(false);
+  return (
+    <div
+      className="menu"
+      onClick={() => {
+        setShow(!show);
+      }}
+    >
+      <span className="icon icon-left">
+        <FontAwesomeIcon icon="user" />
+      </span>
+      Account
+      <span className="icon">
+        <FontAwesomeIcon icon="chevron-down" />
+      </span>
+      {show && (
+        <div className="account">
+          <PopUup />
+        </div>
+      )}
+    </div>
+  );
+};
